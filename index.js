8985edf9d852bbfb3bad78ee68f325a20bdec6ab
aditya singh <adisingh925gmail.com>
2023-06-25 21:25:32 +0530

    create user endpoint created

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const connectToMongo = require('./db')
 const app = express()
 const port = 3000
+connectToMongo()
+
+app.use(express.json())
 
 //Available Routes
 app.use('/api/auth',require('./routes/auth'))
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/User')
 
-router.get('/', (req, res) => {
-    res.json({
-        "message" : "hello world"
-    })
+//Create a new user. Doesn't require authentication
+router.post('/', (req, res) => {
+    console.log(req.body)
+    const user = User(req.body)
+    user.save()
+    res.send("hello")
 })
 
 module.exports = router
