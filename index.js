510bb119f1cee2af075eef928bf1527e20b2e825
aditya singh <adisingh925gmail.com>
2023-07-06 18:41:12 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const connectToMongo = require("./db");
 const app = express();
 const port = process.env.PORT;
